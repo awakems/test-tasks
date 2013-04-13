88014b770711606f34ad69df1f8e8f5ec9bba705
awakems <explo1d@mail.ru>
2013-04-13 21:32:47 +0300

    04_pig_latin - modif  translate func 3 works- 1 fail

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,7 +1,8 @@
 def translate (sen)
- if sen[0]=="a"
-sen+"ay"
+sen.split.map {|x|
+ if (x[0]=="a"|| x[0]=="i"||x[0]=="o"||x[0]=="u"||x[0]=="e")
+x+"ay"
  else 
-sen[1..-1]+sen[0]+"ay"
-end
+x[1..-1]+x[0]+"ay"
+end}.join(" ")
 end
