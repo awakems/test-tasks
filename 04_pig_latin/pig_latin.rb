16ee166643ad70ab9cdc14bab1a73a30dc31331e
awakems <explo1d@mail.ru>
2013-04-13 22:56:59 +0300

    04_pig_latin - modif  translate func 7 works- 1 fail

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -4,6 +4,7 @@ consonants = ('a'..'z').to_a - vowels
 sen.split.map {|x|
 
 if consonants.include?(x[0])
+  return x[2..-1] + x[0..1] + "ay" if ((x[0]=="q") && (x[1]=="u"))
   return x[2..-1] + x[0..1] + "ay" if consonants.include?(x[1]) && vowels.include?(x[2])
   return x[3..-1] + x[0..2] + "ay" if consonants.include?(x[1]) && consonants.include?(x[2])
   x[1..-1] + x[0] + "ay"
