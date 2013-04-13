149702733900eab41ca9d1ff4f92bcfb5b532ec4
awakems <explo1d@mail.ru>
2013-04-13 22:41:45 +0300

    04_pig_latin - modif  translate func 5 works- 1 fail

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,8 +1,12 @@
 def translate (sen)
+vowels = ['a', 'e', 'i', 'o', 'u']
+consonants = ('a'..'z').to_a - vowels
 sen.split.map {|x|
- if (x[0]=="a"|| x[0]=="i"||x[0]=="o"||x[0]=="u"||x[0]=="e")
-x+"ay"
- else 
-x[1..-1]+x[0]+"ay"
-end}.join(" ")
+
+if consonants.include?(x[0])
+  return x[2..-1] + x[0..1] + "ay" if consonants.include?(x[1]) && vowels.include?(x[2])
+  return x[3..-1] + x[0..2] + "ay" if consonants.include?(x[1]) && consonants.include?(x[2])
+  x[1..-1] + x[0] + "ay"
+else x + "ay"
+end }.join(" ")
 end
