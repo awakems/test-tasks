a3719d658428fafaf522d3954a04f64e66ad21b7
awakems <explo1d@mail.ru>
2013-04-19 17:02:36 +0300

    04_pig_latin - edit

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,9 +1,15 @@
 def translate (sen)
   vowels = ['a', 'e', 'i', 'o', 'u']
   consonants = ('a'..'z').to_a - vowels
-  sen.split.map! {|x|
-    if consonants.include?(x[0])
-((x[0]=="q") && (x[1]=="u")) ? x[2..-1] + x[0..1] + "ay": consonants.include?(x[1]) && vowels.include?(x[2]) ? x[2..-1] + x[0..1] + "ay" : consonants.include?(x[1]) && consonants.include?(x[2])? x[3..-1] + x[0..2] + "ay" : x[1..-1] + x[0] + "ay"
-    else x + "ay"
-    end }.join(' ')
+  sen.split.map! {|x| if consonants.include?(x[0])
+
+                          ((x[0]=="q") && (x[1]=="u")) ? x[2..-1] + x[0..1] + "ay":   #умова "qu"
+                          consonants.include?(x[1]) && vowels.include?(x[2]) ? x[2..-1] + x[0..1] + "ay" : #умова початку з двох приголосних
+                          consonants.include?(x[1]) && consonants.include?(x[2])? x[3..-1] + x[0..2] + "ay" : #умова початку з трьох приголосних
+                          x[1..-1] + x[0] + "ay"
+
+                      else 
+                          x + "ay" #починається з голосної
+
+                      end }.join(' ')
 end
