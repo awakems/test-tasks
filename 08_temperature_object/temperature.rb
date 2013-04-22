0a3b2cdb4ce8f7c8af6822a69087fb3e2743310e
awakems <explo1d@mail.ru>
2013-04-22 23:47:35 +0300

    08_temperature_object - 10 exam 1 fail

--- 08_temperature_object/temperature.rb
+++ 08_temperature_object/temperature.rb
@@ -1,2 +1,16 @@
 class Temperature
+
+ def initialize (opt ={})
+   @farenheit =  opt [:f] || opt [:c]*9.0/5.0+32.0
+   @celsius = opt [:c] || (opt [:f]-32.0)*5.0/9.0
+ end
+
+ def in_fahrenheit
+   @farenheit
+ end
+
+ def in_celsius
+   @celsius
+ end
+
 end
