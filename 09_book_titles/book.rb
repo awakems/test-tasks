1b3b2eaab5f2f308d4d80b51e5e8768b2c96766c
awakems <explo1d@mail.ru>
2013-05-18 22:16:26 +0300

    09_book_titles - 9 examples, 0 fail finish

--- 09_book_titles/book.rb
+++ 09_book_titles/book.rb
@@ -2,7 +2,9 @@ class Book
   attr_reader :title
 
   def title=(some_name)
-    @title = some_name.capitalize
+    arr = ['and','over','a', 'an', 'the', 'of', 'in']
+    fin = some_name.gsub(/\w+/) {|match| arr.include?(match) ? match : match.capitalize} 
+    @title = fin[0].capitalize+fin[1..-1]
   end
   
 end
