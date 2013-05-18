c877d5c717003d94322a0521f13882aa9f84ee0a
awakems <explo1d@mail.ru>
2013-05-18 23:52:46 +0300

    10_timer - 5 exaple 0 fail - finish

--- 10_timer/timer.rb
+++ 10_timer/timer.rb
@@ -4,4 +4,20 @@ class Timer
   def initialize (seconds = 0)
     @seconds = seconds
   end
+
+  def time_to_s time
+    time < 10 ? "0" + time.to_s : time.to_s
+  end
+
+  
+  def time_string
+
+    hour   = (@seconds / 3600)
+    minute = (@seconds % 3600) / 60
+    seconds = (@seconds % 60)
+    
+    formated_time = "#{time_to_s(hour)}:#{time_to_s(minute)}:#{time_to_s(seconds)}"
+    
+  end
+ 
 end
