f274a5824746805f8e3b8d74198067cd769ccf5e
awakems <explo1d@mail.ru>
2013-05-23 18:09:36 +0300

    11_dictionary - 4 examp 1 fail

--- 11_dictionary/dictionary.rb
+++ 11_dictionary/dictionary.rb
@@ -1,6 +1,20 @@
 class Dictionary
   attr_accessor :entries
-  def initialize (entries = {})
-    @entries = entries
+
+  def initialize
+    @entries = Hash.new
   end
+  
+  def add (entry)
+    if entry.class == Hash
+      @entries.merge!(entry)
+    else
+      @entries[entry] = nil
+    end
+  end
+ 
+  def keywords
+    @entries.keys.sort
+  end
+
 end
