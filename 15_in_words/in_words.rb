71ad53c9de82dfcc52a075b3e216c19a1289cfff
awakems <explo1d@mail.ru>
2013-06-29 18:22:10 +0300

    15_in_words - 7 exampl 1 fail

--- 15_in_words/in_words.rb
+++ 15_in_words/in_words.rb
@@ -1,26 +1,50 @@
 class Integer
-def in_words
-  if self == 0
-    'zero'
-    elsif self == 1
-      'one'
-    elsif self == 2
-      'two'
-    elsif self == 3
-      'three'
-    elsif self == 4
-      'four'
-    elsif self == 5
-      'five'
-    elsif self == 6
-      'six'
-    elsif self == 7
-      'seven'
-    elsif self == 8
-      'eight'
-    elsif self == 9
-      'nine'
-  end
-  
-end
+  def in_words
+    if self == 0
+      return 'zero'
+    end
+
+
+    numString = ''
+    onesPlace = ['one','two','three','four','five','six','seven','eight','nine']
+    tensPlace = ['ten','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety']
+    teenagers = ['eleven','twelve','thirteen', 'fourteen', 'fifteen','sixteen', 'seventeen', 'eighteen', 'nineteen']
+    left = self
+    write = left / 100
+    left = left - write * 100
+
+    if write > 0
+      hundreds = write.in_words
+      numString = numString + hundreds + ' hundred'
+        if left > 0
+          numString = numString + ' '
+        end
+    end
+
+
+   write = left / 10 
+   left = left - write * 10
+
+     if write > 0
+        if ((write == 1) and (left > 0))
+          numString = numString + teenagers[left - 1]
+          left = 0
+        else
+          numString = numString + tensPlace[write - 1]
+        end
+        if left > 0
+           numString = numString + ' '
+        end
+     end
+
+    write = left 
+    left = 0
+
+    if write > 0
+      numString = numString + onesPlace[write-1]
+    end
+
+  numString
+
+  end 
 end
